71f73431d103f4752a0964d962f0f855c87cf9f0
mori618 <148190402+mori618@users.noreply.github.com>
2023-12-05 08:10:01 +0000

    12/5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/top'
+  get 'home/about'
+  root to:  "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ practice-variable/app.js
@@ -0,0 +1,8 @@
+let i;
+let int2 = 2;
+let count = 0
+
+for(i = 1; i < 10; i++){
+  count = count + i
+}
+alert(count);
